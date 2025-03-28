0e68f6a6f131d9e5fb464f8b9259bf8685da950e
lmfavaretto <55554697+lmfavaretto@users.noreply.github.com>
2025-03-28 16:59:07 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -18,7 +18,15 @@ function startClient() {
     authStrategy: new LocalAuth(),
     puppeteer: {
       headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-accelerated-2d-canvas',
+        '--no-zygote',
+        '--single-process',
+        '--disable-gpu'
+      ]
     }
   });
 
